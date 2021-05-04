80e66a9be4ad44867f58becce69653a7ebd0b860
blancadennis <lblancadennis@gmail.com>
2021-05-03 20:38:07 -0500

    style in login and menus and buttons working

--- burgerqueenapp/src/components/Login.jsx
+++ burgerqueenapp/src/components/Login.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import logoBurguerQueen from '../assets/logoBurguerQueen.png'
+import {useHistory} from 'react-router-dom'
+
 
 
 const Login = () => {
 
+    const history = useHistory();
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const LoginUser = (e) =>{
+        e.preventDefault();
+        console.log(email, password)
+        history.push('/menus')
+        
+    }
+
+
 
     return (
         <div>
-            <h3> Click para iniciar sesión (: </h3>
+            <div id="logoheader">
+                <img src={logoBurguerQueen}></img>
+            </div>
+            <p id="textPage">Iniciar Sesión</p>
+
+            <form onSubmit={LoginUser} id="formLogin">
+                <div className="inputsLogin">
+                    <input className="email" placeholder="email" type="text" onChange={(e)=>setEmail(e.target.value)}></input>
+                    <br></br>
+                    <input className="password" placeholder="contraseña" type="password" onChange={(e)=>setPassword(e.target.value)}></input>
+                </div>
+                <div>     
+                    <input className="buttonLogin" type="submit" value="iniciar sesión"></input>
+                </div>
+            </form>
         </div>
     )
 };
