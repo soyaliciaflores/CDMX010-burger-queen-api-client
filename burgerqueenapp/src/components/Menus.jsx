143948ecf1fe3250477b30fad5f6fc9e08d33d9e
Alicia Flores <floresalvarado.alicia@gmail.com>
2021-05-04 13:16:11 -0500

    with interfaz login and menus

--- burgerqueenapp/src/components/Menus.jsx
+++ burgerqueenapp/src/components/Menus.jsx
@@ -1,13 +1,49 @@
 import React from 'react';
+import logoBurguerQueen from '../assets/logoBurguerQueen.png'
+import {useHistory} from 'react-router-dom'
 
 
 const Menus = () => {
 
+    const history = useHistory();
+
+    const ButtonToBreakfast = (e) =>{
+        e.preventDefault();
+        console.log("el boton sirve BREAKFAST")
+        history.push('/breakfast')
+        
+    }
+
+    const ButtonToDinner = (e) =>{
+        e.preventDefault();
+        console.log("el boton sirve DINNER")
+        history.push('/dinner')
+        
+    }
+
+    const Logout = (e) =>{
+        e.preventDefault();
+        console.log("el boton sirve logout")
+        history.push('/')
+        
+    }
 
     return (
         <div>
-            <h3> Soy los menus </h3>
+            <div id="logoheaderMenu">
+                <img src={logoBurguerQueen}></img>
+            </div>
+            <div>     
+                <input className="buttonFood" type="submit" value="Menú desayuno" onClick={ButtonToBreakfast}></input>
+                <br></br>
+                <input className="buttonFood" type="submit" value="Menú comida"onClick={ButtonToDinner}></input>
+            </div>
+            <div>
+                <input className="buttonLogout" type="submit" value="Cerrar Sesión" onClick={Logout}></input>
+            </div>
+
         </div>
+
     )
 };
 
